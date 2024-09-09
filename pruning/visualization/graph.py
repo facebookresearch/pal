adc2ee71e2a16002fbada3518c929a394a06de85
Wes Bouaziz <wesbz@fb.com>
2024-09-09 10:28:18 +0000

    Change the graph viz for better understanding

--- pruning/visualization/graph.py
+++ pruning/visualization/graph.py
@@ -42,16 +42,46 @@ def build_1L_graph(N):
     """
     G = nx.Graph()
 
-    G.add_nodes_from([f"0{i}" for i in range(N)])
-    G.add_node("1")
+    G.add_nodes_from([rf"$x_{{{i}}}$" for i in range(N)])
+    G.add_node(r"$q$")
 
     # Add edges between nodes in different sets
     for i in range(N):
-        G.add_edge(f"0{i}", "1")
+        G.add_edge(rf"$x_{{{i}}}$", r"$q$")
 
     pos = {}
     for i in range(N):
-        pos[f"0{i}"] = (i, 0)
-    pos["1"] = (N / 2, -1)
+        pos[rf"$x_{{{i}}}$"] = (i, 0)
+    pos[r"$q$"] = (N / 2, -1)
+
+    return G, pos
+
+
+def build_1L_graph_tokens(N, tokens):
+    """
+    Build attention visualization graph with N tokens per layers.
+
+    Parameters
+    ----------
+    N : int
+        Number of tokens per layer.
+    """
+    G = nx.Graph()
+
+    G.add_nodes_from([rf"$x_{{{i}}}$" for i in range(N)])
+    # G.add_nodes_from([f"{i}" for i in tokens],)
+    for i, j in enumerate(tokens):
+        G.add_node(rf"$y_{{{i}}}$", label=j)
+    G.add_node(r"$q$")
+
+    # Add edges between nodes in different sets
+    for i in range(N):
+        G.add_edge(rf"$y_{{{i}}}$", r"$q$")
+
+    pos = {}
+    for i in range(N):
+        pos[rf"$y_{{{i}}}$"] = (i, 0)
+        pos[rf"$x_{{{i}}}$"] = (i, 0.1)
+    pos[r"$q$"] = (N / 2, -1)
 
     return G, pos
