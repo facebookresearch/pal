3fa4b0d22ff5f356fa075dd76f745836f1c31065
Vivien Cabannes <vivien.cabannes@gmail.com>
2024-09-11 12:30:39 -0400

    dataloader

--- src/factorization/models/softmax_model.py
+++ src/factorization/models/softmax_model.py
@@ -1,5 +1,12 @@
 """
 Simple Transformer-like Architecture.
+
+License
+-------
+This source code is licensed under the CC license found in the LICENSE file
+in the root directory of this source tree.
+
+@ 2024, Meta
 """
 
 import math
