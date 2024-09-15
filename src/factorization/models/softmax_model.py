3d560d95e09ed9fda09d6ccd01c4e4a0bc479c1e
Vivien Cabannes <vivien.cabannes@gmail.com>
2024-09-15 08:26:59 -0400

    update docstring

--- src/factorization/models/softmax_model.py
+++ src/factorization/models/softmax_model.py
@@ -7,10 +7,6 @@ This source code is licensed under the CC license found in the LICENSE file
 in the root directory of this source tree.
 
 @ 2024, Meta
-
-TODO
-----
-Remove dependency on MuP
 """
 
 import math
