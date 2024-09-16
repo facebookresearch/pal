e748082f3e40a7fca0949d40e07c42f41403c417
Vivien Cabannes <vivien.cabannes@gmail.com>
2024-09-16 05:17:28 -0400

    better config handling

--- src/factorization/config.py
+++ src/factorization/config.py
@@ -9,6 +9,25 @@ in the root directory of this source tree.
 @ 2024, Meta
 """
 
+from configparser import ConfigParser
 from pathlib import Path
 
-SAVE_DIR = Path(__file__).resolve().parents[2] / "pruning" / "results"
+import torch
+
+# Device
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+ROOT_DIR = Path(__file__).parents[2]
+
+
+# Priority to user config
+config: dict[str, dict] = ConfigParser()
+config.read(ROOT_DIR / "default_config.ini")
+config.read(ROOT_DIR / "user_config.ini")
+
+# Paths
+# Priority to absolute over relative paths
+for name in config["Relative Paths"]:
+    globals()[name.upper()] = ROOT_DIR / config["Relative Paths"][name]
+
+for name in config["Absolute Paths"]:
+    globals()[name.upper()] = Path(config["Absolute Paths"][name])
