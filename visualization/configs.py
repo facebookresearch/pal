eeb36dafa41c03eefb850420c74c20f4150bc1f7
Vivien Cabannes <vivien.cabannes@gmail.com>
2024-09-18 13:31:13 +0000

    license

--- visualization/configs.py
+++ visualization/configs.py
@@ -1,3 +1,14 @@
+"""
+Utils
+
+License
+-------
+This source code is licensed under the CC license found in the LICENSE file
+in the root directory of this source tree.
+
+@ 2024, Meta
+"""
+
 import json
 import logging
 
