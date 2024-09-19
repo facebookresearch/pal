ca8f8876fedf71b88c563984dd942079426792b0
Vivien Cabannes <vivien.cabannes@gmail.com>
2024-09-19 09:48:19 +0000

    script cosmetic fix

--- visualization/bash/pipeline.sh
+++ visualization/bash/pipeline.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 
 # This file showcases a typical pipeline to run the various scripts.
-# Train is launch manually, it could be launched with a slurm launcher as well, c.f. `train_launcher.sh`.
+# Slurm launcher examples are also provided.
 
-python ../train.py run --seed 0 --ffn_dim 10 --save-ext tmp --save-weights True
-python ../train.py run --seed 0 10 --ffn-dim 16 --save-ext tmp --save-weights True
-python ../train.py run --seed 0 10 --ffn-dim 16 --batch-size 20 --save-ext tmp --save-weights True
+python ../train.py run --seed 0 --nb-epochs 100 --ffn_dim 10 --save-ext tmp --save-weights True
+python ../train.py run --seed 0 --nb-epochs 100 --ffn-dim 16 --save-ext tmp --save-weights True
+python ../train.py run --seed 0 --nb-epochs 100 --ffn-dim 16 --batch-size 20 --save-ext tmp --save-weights True
 python ../configs.py --save-ext tmp
 python ../analysis.py losses --save-ext tmp --file-format png
 
@@ -14,5 +14,6 @@ while read line; do
   python ../visualization.py frame --epoch 0 --file-format png --save-ext tmp --unique-id $name
 done < ../../savings/configs/tmp.jsonl
 
-sbatch ./visu_launcher.sh
-# python ./visualization.py all_aggregate --save-ext tmp --num-tasks $SLURM_ARRAY_TASK_COUNT --task-id $SLURM_ARRAY_TASK_ID
+
+python ../visualization.py all_animation --save-ext tmp --num-tasks-per-videos 10 --title-key batch_size
+python ../visualization.py all_aggregate --save-ext tmp
--- visualization/bash/visu_launcher.sh
+++ visualization/bash/visu_launcher.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
 # Logging configuration
-#SBATCH --job-name=visu
-#SBATCH --output=~/%u/visu/%j-%a.out
-#SBATCH --error=~/%u/visu/%j-%a.err
+#SBATCH --job-name=video
+#SBATCH --output=~/%u/video/%j-%a.out
+#SBATCH --error=~/%u/video/%j-%a.err
 
 # Job specification
 #SBATCH --time=5:00:00
@@ -11,8 +11,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=10
-#SBATCH --gpus-per-node=1
-#SBATCH --array=1-90
+#SBATCH --gpus-per-node=0
+#SBATCH --array=1-300
 
 # TODO: make it nicer
-python ./visualization.py all_animation --save-ext tmp --num-tasks $SLURM_ARRAY_TASK_COUNT --num-tasks-per-videos 30 --task-id $SLURM_ARRAY_TASK_ID --title-key batch_size
+python ./visualization.py all_animation --save-ext tmp --num-tasks $SLURM_ARRAY_TASK_COUNT --num-tasks-per-videos 100 --task-id $SLURM_ARRAY_TASK_ID --title-key batch_size
