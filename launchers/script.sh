f21b9fa52f6e37c1f4b68642cf5aae767b54494f
Vivien Cabannes <vivien.cabannes@gmail.com>
2024-10-03 16:59:51 +0000

    cosmetic change

--- launchers/script.sh
+++ launchers/script.sh
@@ -3,6 +3,6 @@
 CODE_PATH="/private/home/vivc/code/memory-theory"
 
 # slurm launcher
-# sbatch ${CODE_PATH}/launchers/compression.sh
+sbatch ${CODE_PATH}/launchers/compression.sh
 
-python ${CODE_PATH}/factorization/configs.py aggregate debug_experiment
+# python ${CODE_PATH}/factorization/configs.py aggregate debug_experiment
